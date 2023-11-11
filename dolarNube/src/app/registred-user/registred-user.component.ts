b4c3240d6c37e93fb5f801bb399f0da718ee5245
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2023-11-11 11:57:54 -0300

    cognito implementation

---
+++ dolarNube/src/app/registred-user/registred-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registred-user',
+  templateUrl: './registred-user.component.html',
+  styleUrls: ['./registred-user.component.css']
+})
+export class RegistredUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
